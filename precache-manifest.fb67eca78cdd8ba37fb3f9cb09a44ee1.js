c9287c51c99c06fe3db5d95e13832dd9bf02aaa5
HamidrezaMaharlu <maharlu.hamidreza@gmail.com>
2020-08-16 01:41:48 +0430

    Updates

---
+++ precache-manifest.fb67eca78cdd8ba37fb3f9cb09a44ee1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a089b9296ecce714b5b",
+    "url": "/TodoList/static/css/main.ffe8dfe9.chunk.css"
+  },
+  {
+    "revision": "5a089b9296ecce714b5b",
+    "url": "/TodoList/static/js/main.5a089b92.chunk.js"
+  },
+  {
+    "revision": "1d3811e39960ac94bd28",
+    "url": "/TodoList/static/js/1.1d3811e3.chunk.js"
+  },
+  {
+    "revision": "e5fd00049e7ac7cce407",
+    "url": "/TodoList/static/js/runtime~main.e5fd0004.js"
+  },
+  {
+    "revision": "48cb0ca0582317ecc75dd7de2c858e85",
+    "url": "/TodoList/index.html"
+  }
+];
